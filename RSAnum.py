3552ac2879b8ca0d152e6dd12f3c7a9b296e7b7c
xbuchaj <jakubkobucha@gmail.com>
2023-01-19 13:55:33 +0100

    Write a function for generate list (lenght of list is n) of prime numbers.

--- RSAnum.py
+++ RSAnum.py
@@ -2,3 +2,19 @@ import random
 
 def nBitRandomNum(n):
     return(random.randrange(2**(n-1)+1, 2**n-1))
+
+def nPrimeNumber(n):
+    primeNumbers = []
+    if n > 0:
+        primeNumbers.append(2)
+        i = 3
+    while len(primeNumbers) < n:
+        flag = True
+        for j in range(2, i - 1):
+            if i % j == 0:
+                flag = False
+                break
+        if flag == True:
+            primeNumbers.append(i)
+        i += 1
+    return primeNumbers
