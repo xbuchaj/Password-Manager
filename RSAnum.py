56bc59a01072518c178f082e64b47b0a708fe702
xbuchaj <jakubkobucha@gmail.com>
2023-01-19 14:02:49 +0100

    Write a function for Miller-Rabin test of primality which return True if number is prime.

--- RSAnum.py
+++ RSAnum.py
@@ -24,4 +24,26 @@ def LowLevelPrimalityTest(candidate, nPrimes):
     for i in range(len(PrimeNumbers)):
         if candidate % PrimeNumbers[i] == 0:
             return False
-    return True            
+    return True
+
+def MillerRabinTest(candidate, k):
+    maxDivisionsByTwo = 0
+    ec = candidate-1
+    while ec % 2 == 0:
+        ec >>= 1
+        maxDivisionsByTwo += 1
+    assert(2**maxDivisionsByTwo * ec == candidate-1)
+ 
+    def trialComposite(round_tester):
+        if pow(round_tester, ec, candidate) == 1:
+            return False
+        for i in range(maxDivisionsByTwo):
+            if pow(round_tester, 2**i * ec, candidate) == candidate-1:
+                return False
+        return True
+ 
+    for i in range(k):
+        round_tester = random.randrange(2, candidate)
+        if trialComposite(round_tester):
+            return False
+    return True           
