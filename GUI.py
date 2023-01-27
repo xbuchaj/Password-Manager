380c24e5e6e0e1c85cd2ff25b0f550ebb2bb61f3
xbuchaj <jakubkobucha@gmail.com>
2023-01-27 11:25:54 +0100

    Make move funciont in the log in (GUI) with UP and DOWN keys.

--- GUI.py
+++ GUI.py
@@ -16,19 +16,22 @@ def logIn():
     print(terminal.move_xy((terminal.width // 2) - 8, (terminal.height // 2) + 1) +  "USERNAME:")
     print(terminal.move_xy((terminal.width // 2) - 8, (terminal.height // 2) + 3) +  "PASSWORD:")
     print(terminal.move_xy((terminal.width // 2) - 2, (terminal.height // 2) + 5) +  "DONE")
-    print(terminal.move_xy((terminal.width // 2) - 17, terminal.height) +  "Tab: MOVE, Enter: SELECT, ESC: Quit")
+    print(terminal.move_xy((terminal.width // 2) - 17, terminal.height) +  "↑↓: MOVE, Enter: SELECT, ESC: Quit")
 
     # Variable (int) for position where is cursor
     pointer = 1
     while True:
         # Commands for drawing the cursor
         if pointer == 1:
+            print(terminal.move_xy((terminal.width // 2) + 2 + len(password), (terminal.height // 2) + 2) +  " ")
             print(terminal.move_xy((terminal.width // 2) - 2, (terminal.height // 2) + 4) +  "DONE")
             print(terminal.move_xy((terminal.width // 2) + 2 + len(username), terminal.height // 2) +  "█")
         elif pointer == 2:
+            print(terminal.move_xy((terminal.width // 2) - 2, (terminal.height // 2) + 4) +  "DONE")
             print(terminal.move_xy((terminal.width // 2) + 2 + len(username), terminal.height // 2) +  " ")
             print(terminal.move_xy((terminal.width // 2) + 2 + len(password), (terminal.height // 2) + 2) +  "█")
         else:
+            print(terminal.move_xy((terminal.width // 2) + 2 + len(username), terminal.height // 2) +  " ")
             print(terminal.move_xy((terminal.width // 2) + 2 + len(password), (terminal.height // 2) + 2) +  " ")
             print(terminal.cornflowerblue + terminal.on_white)
             print(terminal.move_xy((terminal.width // 2) - 2, (terminal.height // 2) + 4)  +  "DONE")
@@ -55,19 +58,32 @@ def logIn():
                         password = password[0: len(password) - 1]
                         print(terminal.move_xy((terminal.width // 2) + 3 + len(password), (terminal.height // 2) + 2) +  " ")
 
-                # If was press the TAB key or ENTER key
-                elif pointer < 3:
-                    if (val.code == 512) or (val.code == 343):
+                # If was press ENTER key
+                elif val.code == 343:
+                    if pointer < 3:
                         pointer += 1
-                elif pointer == 3:
-                    if val.code == 343:
+                    elif pointer == 3:
                         return username, password
-                    elif val.code == 512:
-                        pointer = 1
-                if pointer > 3:
-                    pointer = 1
 
-            # If wasn press key with letter
+                # If was press UP key
+                elif val.code == 259:
+                    if pointer == 1:
+                        pointer = 3
+                    elif pointer == 2:
+                        pointer = 1
+                    elif pointer == 3:
+                        pointer = 2
+                
+                # If was press DOWN key
+                elif val.code == 258:
+                    if pointer == 1:
+                        pointer = 2
+                    elif pointer == 2:
+                        pointer = 3
+                    elif pointer == 3:
+                        pointer = 1  
+
+            # If was press key with letter
             else:
                 if pointer == 1:
                     username = username + format(val)
