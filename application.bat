ff33c275a1f4aa419c95fd2a5d7e705dd1ec6593
xbuchaj <jakubkobucha@gmail.com>
2023-02-25 11:13:07 +0100

    Set default type of font and font size.

--- application.bat
+++ application.bat
@@ -1,5 +1,55 @@
-ECHO OFF
+@echo off &setlocal
+
+call :init_setfont
+
+%setfont% 20 Lucida Console
+
 cls
 TITLE Password Manager
 py main.py
 exit
+
+:init_setfont
+:: - BRIEF -
+::  Get or set the console font size and font name.
+:: - SYNTAX -
+::  %setfont% [fontSize [fontName]]
+::    fontSize   Size of the font. (Can be 0 to preserve the size.)
+::    fontName   Name of the font. (Can be omitted to preserve the name.)
+:: - EXAMPLES -
+::  Output the current console font size and font name:
+::    %setfont%
+::  Set the console font size to 14 and the font name to Lucida Console:
+::    %setfont% 14 Lucida Console
+setlocal DisableDelayedExpansion
+set setfont=for /l %%# in (1 1 2) do if %%#==2 (^
+%=% for /f "tokens=1,2*" %%- in ("? ^^!arg^^!") do endlocal^&powershell.exe -nop -ep Bypass -c ^"Add-Type '^
+%===% using System;^
+%===% using System.Runtime.InteropServices;^
+%===% [StructLayout(LayoutKind.Sequential,CharSet=CharSet.Unicode)] public struct FontInfo{^
+%=====% public int objSize;^
+%=====% public int nFont;^
+%=====% public short fontSizeX;^
+%=====% public short fontSizeY;^
+%=====% public int fontFamily;^
+%=====% public int fontWeight;^
+%=====% [MarshalAs(UnmanagedType.ByValTStr,SizeConst=32)] public string faceName;}^
+%===% public class WApi{^
+%=====% [DllImport(\"kernel32.dll\")] public static extern IntPtr CreateFile(string name,int acc,int share,IntPtr sec,int how,int flags,IntPtr tmplt);^
+%=====% [DllImport(\"kernel32.dll\")] public static extern void GetCurrentConsoleFontEx(IntPtr hOut,int maxWnd,ref FontInfo info);^
+%=====% [DllImport(\"kernel32.dll\")] public static extern void SetCurrentConsoleFontEx(IntPtr hOut,int maxWnd,ref FontInfo info);^
+%=====% [DllImport(\"kernel32.dll\")] public static extern void CloseHandle(IntPtr handle);}';^
+%=% $hOut=[WApi]::CreateFile('CONOUT$',-1073741824,2,[IntPtr]::Zero,3,0,[IntPtr]::Zero);^
+%=% $fInf=New-Object FontInfo;^
+%=% $fInf.objSize=84;^
+%=% [WApi]::GetCurrentConsoleFontEx($hOut,0,[ref]$fInf);^
+%=% If('%%~.'){^
+%===% $fInf.nFont=0; $fInf.fontSizeX=0; $fInf.fontFamily=0; $fInf.fontWeight=0;^
+%===% If([Int16]'%%~.' -gt 0){$fInf.fontSizeY=[Int16]'%%~.'}^
+%===% If('%%~/'){$fInf.faceName='%%~/'}^
+%===% [WApi]::SetCurrentConsoleFontEx($hOut,0,[ref]$fInf);}^
+%=% Else{(''+$fInf.fontSizeY+' '+$fInf.faceName)}^
+%=% [WApi]::CloseHandle($hOut);^") else setlocal EnableDelayedExpansion^&set arg=
+endlocal &set "setfont=%setfont%"
+if !!# neq # set "setfont=%setfont:^^!=!%"
+exit /b
